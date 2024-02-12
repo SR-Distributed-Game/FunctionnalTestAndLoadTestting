c55ac9c3940d3341fec0fbb25adb6c67d9f74488
Yann Paillard <yann.paillard@etudiant.univ-rennes1.fr>
2024-02-12 20:24:36 +0100

    fix test

--- FonctionnalTest/test_game.py
+++ FonctionnalTest/test_game.py
@@ -5,58 +5,58 @@ import unittest
 
 class TestWebSocketConnectionGame(unittest.TestCase):
     def test_1_game_websocketConnection(self):
+
         ws_url = "ws://localhost:8080/game"
         message = """{"Type":"ConnectSucces","ClientID":-1,"Metadata":{"playername":"OUAIIIIIIIS"},"RoomID":-1}"""
         ws = websocket.create_connection(ws_url)
         try:
             ws.send(message)
             response = ws.recv()
-            print(response)
-            self.assertEqual(response, """{"Type":"ConnectSucces","Metadata":{"clientID":0},"ClientId":"-2","RoomId":"-1"}""")
+
+            response_data = json.loads(response)
+            response_data["Metadata"]["clientID"] = "DYNAMIC"
+
+            modified_response = json.dumps(response_data, sort_keys=True)
+
+            print(modified_response)
+
+            expected_response_data = json.loads("""{"Type":"ConnectSucces","Metadata":{"clientID": "DYNAMIC"},"ClientId":"-2","RoomId":"-1"}""")
+            expected_response = json.dumps(expected_response_data, sort_keys=True)
+            self.assertEqual(modified_response, expected_response)
         finally:
             ws.close()
 
     def test_2_game_websocketJoinRoom(self):
+
         ws_url = "ws://localhost:8080/game"
         message = """{"Type":"JoinRoom","ClientID":0,"Metadata":{"clientID":0},"RoomID":-1}"""
         ws = websocket.create_connection(ws_url)
         try:
             ws.send(message)
             response = ws.recv()
-            print(response)
-            self.assertEqual(response, """{"Type":"JoinRoom","Metadata":{"leaderboard":{"0":0},"players":{"0":"OUAIIIIIIIS"}},"ClientId":"-2","RoomId":"-1"}""")
-        finally:
-            ws.close()
 
-    import json
-
-    def test_3_game_websocketCreatePlayer(self):
-        ws_url = "ws://localhost:8080/game"
-        message = """{"Type":"SpawnObject","ClientID":0,"Metadata":{"objectData":{"transform":{"position":{"x":100,"y":100},"scale":{"x":0,"y":0},"rotation":0},"name":"OUAIIIIIIIS","id":-1,"points":0,"speed":5,"clientID":0,"Type":"player"}},"RoomID":-1}"""
-        ws = websocket.create_connection(ws_url)
-        try:
-            ws.send(message)
-            response = ws.recv()
             response_data = json.loads(response)
-            response_data["Metadata"]["objectData"]["id"] = "DYNAMIC"
+            response_data["Metadata"]["players"] = "DYNAMIC"
+
             modified_response = json.dumps(response_data, sort_keys=True)
 
-            expected_response_data = json.loads(
-                """{"Type":"SpawnObject","Metadata":{"objectData":{"transform":{"rotation":0,"scale":{"x":20,"y":20},"position":{"x":100,"y":100}},"Type":"player","clientID":0,"name":"OUAIIIIIIIS","id":"DYNAMIC","speed":5,"points":0}},"ClientId":"-2","RoomId":"-1"}""")
+            expected_response_data = json.loads("""{"Type":"JoinRoom","Metadata":{"leaderboard":{"0":0},"players": "DYNAMIC"},"ClientId":"-2","RoomId":"-1"}""")
             expected_response = json.dumps(expected_response_data, sort_keys=True)
 
+            print(response)
             self.assertEqual(modified_response, expected_response)
         finally:
             ws.close()
 
-    def test_4_game_websocketMovePlayer(self):
+
+    def test_3_game_websocketMovePlayer(self):
         ws_url = "ws://localhost:8080/game"
-        message = """{"Type":"UpdateObject","ClientID":0,"Metadata":{"objectData":{"transform":{"position":{"x":100,"y":100},"scale":{"x":20,"y":20},"rotation":0},"name":"OUAIIIIIIIS","id":5,"points":0,"speed":5,"clientID":0,"Type":"player"}},"RoomID":-1}"""
+        message = """{"Type":"UpdateObject","ClientID":0,"Metadata":{"objectData":{"transform":{"position":{"x":572.2338528885318,"y":783.5543260455736},"scale":{"x":20,"y":20},"rotation":0},"futurtransform":{"position":{"x":572.2338528885318,"y":783.5543260455736},"scale":{"x":20,"y":20},"rotation":0},"name":"Payll","id":107,"tag":"player","points":0,"speed":0.2,"hasBeenEaten":false,"clientID":0,"Type":"player"}},"RoomID":-1}"""
         ws = websocket.create_connection(ws_url)
         try:
             ws.send(message)
             response = ws.recv()
-
+            print(response)
             # Analyser la réponse JSON
             response_data = json.loads(response)
 
@@ -70,7 +70,7 @@ class TestWebSocketConnectionGame(unittest.TestCase):
             modified_response = json.dumps(response_data, sort_keys=True)
             print(modified_response)
             expected_response_data = json.loads(
-                """{"ClientId": "-2", "Metadata": {"objectData": {"SerializationTest": "TEST FOR SERIALIZATION", "Type": "fruit", "id": "DYNAMIC", "name": "fruit", "transform": {"position": {"x": "DYNAMIC", "y": "DYNAMIC"}, "rotation": 0, "scale": {"x": 10, "y": 10}}}}, "RoomId": "-1", "Type": "SpawnObject"}""")
+                """{"ClientId": "-2", "Metadata": {"objectData": {"SerializationTest": "TEST FOR SERIALIZATION", "Type": "fruit", "id": "DYNAMIC", "lifeTime": 1, "name": "fruit", "tag": "fruit", "transform": {"position": {"x": "DYNAMIC", "y": "DYNAMIC"}, "rotation": 0, "scale": {"x": 10, "y": 10}}}}, "RoomId": "-1", "Type": "SpawnObject"}""")
             expected_response = json.dumps(expected_response_data, sort_keys=True)
 
             self.assertEqual(modified_response, expected_response)
